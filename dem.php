099e33edbe236e8398010697ce53ff5ea28705cd
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-02 14:55:59 +0530

    Add files via upload

---
+++ dem.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Switch statements in PHP</h1>
+    <?php    
+        
+        $n = 3;
+
+        switch($n){
+            case 1:
+                echo 'one';
+                break;
+            case 2:
+                echo 'two';
+                break;
+            case 3:
+                echo 'three';
+                break;  
+            case 4:
+                echo 'four';
+                break;        
+        }
+    ?>
+</body>
+</html>
